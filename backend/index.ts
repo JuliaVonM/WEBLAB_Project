0e437ec90afc38394e3a9595abe7f0d5e70316ac
JuliaVonM <36076341+JuliaVonM@users.noreply.github.com>
2025-03-04 09:10:47 +0100

    Add logging for login

--- backend/index.ts
+++ backend/index.ts
@@ -4,7 +4,5 @@ import {connectToDB} from "./src/config/database";
 connectToDB()
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    /* eslint-disable no-console */
     console.log(`Listening: http://localhost:${port}`);
-    /* eslint-enable no-console */
 });
--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -2,6 +2,7 @@ import {Request, Response} from "express";
 import {User} from "../models/user.model";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
+import logger from "../logging/logger";
 
 export const login = (req: Request, res: Response) => {
     const {email, password} = req.body;
@@ -9,10 +10,12 @@ export const login = (req: Request, res: Response) => {
     User.findOne({email: email})
     .then((user) => {
         if (!user) {
+            logger.info(`Failed login try: ${email} (User not found)`);
             return res.status(400).send('User not found');
         }
 
         if (!bcrypt.compareSync(password, user.password)) {
+            logger.info(`Failed login try: ${email} (False password)`);
             return res.status(400).send('False password');
         }
 
@@ -21,9 +24,13 @@ export const login = (req: Request, res: Response) => {
             role: user.role
         }, 'secretkey', {expiresIn: '1h'});
 
+        logger.info(`Successful login: ${email}`);
         res.json({token});
     })
-    .catch((error) => res.status(500).json({message: "Server error", error}));
+    .catch((error) => {
+        logger.error(`Login error for ${email}: ${error.message}`);
+        res.status(500).json({message: "Server error", error});
+    });
 };
 
 // Get all users
